df354b3d9be82734d0810888ed64a5eaa33a2201
Vedantkhairnar729 <vedantkhairnar041@gmail.com>
2026-08-03 10:46:32 +0530

    com_1

---
+++ 1) Java Basics/1) Variable and Data Types/Datatypes.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Datatypes{
+public static void main(String[] args){
+
+//datatypes
+
+
+
+}
+}
---
+++ pro.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class pro
+{
+    public static void main(String args[]){
+        Scanner V1 = new Scanner(System.in);
+      
+//   Methods	                             Description
+
+// nextBoolean()	            Reads a boolean value from the user
+// nextByte()	                Reads a byte value from the user
+// nextDouble()	                Reads a double value from the user
+// nextFloat()	                Reads a float value from the user
+// nextInt()	                Reads a int value from the user
+// nextLine()	                Reads a String value from the user
+// nextLong()	                Reads a long value from the user
+// nextShort()	                Reads a short value from the user
+
+    //Ex : int = nextInt()
+    int a = V1.nextInt();
+    int b = V1.nextInt();
+    System.out.println(ans+(a*b));
+
+
+    //Ex : String = nextLine()
+    System.out.println("take name");
+    String first = V1.nextLine();
+    String last = V1.nextLine();
+    System.out.println(first+" "+ last);
+
+    
+
+    }
+
+
+
+}
