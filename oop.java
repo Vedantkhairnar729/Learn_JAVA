9e1f05068a4a47c17796e45c18e2a8366a49a6f4
Vedantkhairnar729 <vedantkhairnar041@gmail.com>
2026-08-14 10:30:14 +0530

    com 6

--- oop.java
+++ oop.java
@@ -21,3 +21,10 @@ class oop{
     }
 }
 
+    // A constructor is a special type of method that is used to initialize an object.
+    // A constructor is automatically called when an object is created.
+    // A constructor must have the same name as the class.
+    // A constructor does not have a return type, not even void.
+    // There are two common types of constructors:
+    // Default Constructor – A constructor that does not have any parameters.
+    // Parameterized Constructor – A constructor that accepts one or more parameters.  
